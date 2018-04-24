b1ee2a5ef3960ada03d72bbd81038bf20454f5ee
Narcisa Ana Maria Vasile <narcisaanamaria12@gmail.com>
2018-04-24 16:27:51 +0300

    cpu: avr: Add support for timer2
    Timer2 is used to wake sparrow from sleep.
    timer2_init() is used to intialize the timer and sparrow_sleep()
    puts the device to sleep and wakes it up when timer2's
    counter overflows.
    
    Signed-off-by: Narcisa Ana Maria Vasile <narcisaanamaria12@gmail.com>

---
+++ cpu/avr/timer2.c
@@ -0,0 +1,38 @@
+#include <avr/interrupt.h>
+#include <avr/sleep.h>
+
+#include "sys/etimer.h"
+#include "timer2.h"
+
+ISR(TIMER2_OVF_vect)
+{
+  tot_overflow++;
+  etimer_request_poll();
+}
+
+void timer2_init()
+{
+    //PRR0 &= ~(1 << PRTIM2);
+    tot_overflow = 0;
+    // Disable interrupts
+    TIMSK2 = 0;
+    ASSR = 1 << AS2;
+    TIMSK2 = 1 << TOIE2;
+    TCCR2A = 0;
+    TCCR2B = 0x07; //prescaler 1024 => 8.16 seconds
+    do {} while (ASSR & (1 << TCR2BUB));
+}
+
+void sparrow_sleep() {
+        //Disable transciever
+        TRXPR = 1 << SLPTR;
+
+        set_sleep_mode(SLEEP_MODE_PWR_SAVE);
+
+        //Initialize counter
+        TCNT2 = 0;
+        do {} while (ASSR & (1 << TCN2UB));
+        sleep_enable();
+        sleep_cpu();
+        sleep_disable();
+}
---
+++ cpu/avr/timer2.h
@@ -0,0 +1,9 @@
+#ifndef __TIMER2_H__
+#define __TIMER2_H__
+
+volatile uint8_t tot_overflow;
+
+void timer2_init();
+void sparrow_sleep();
+
+#endif
